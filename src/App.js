030a10913bb4cf992ac0ac4afcc3e987f4547515
sajibur-rahman <sajiburr21@gmail.com>
2022-04-16 08:36:26 +0600

    firebase setup is done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-     <h1>welcome to red onion</h1>
+     <h1 className="text-3xl font-bold underline text-center">welcome to red onion</h1>
     </div>
   );
 }
