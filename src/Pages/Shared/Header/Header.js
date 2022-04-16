9ae68086a585474cc850dfe344ecfad15b9b88ab
sajibur-rahman <sajiburr21@gmail.com>
2022-04-16 09:29:29 +0600

    header component is done

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import brandLogo from '../../../assets/images/logo2.png';
+
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is header page</h1>
-        </div>
+        <header className='max-w-7xl mx-auto'>
+            <nav className='flex items-center justify-between h-5 px-10 py-10'>
+                <div>
+                    <img className='w-32' src={brandLogo} alt="brandlogo" />
+                </div>
+                <div>
+                    <ul className='flex justify-between items-center'>
+                        <li className='mr-4'>
+                            <NavLink to="/cart">cart</NavLink>
+                        </li>
+                        <li className='mr-4'>
+                            <NavLink to="/login">Logn</NavLink>
+                        </li>
+                        <li className='mr-4 px-10 py-2 bg-red-700 text-white rounded-full'>
+                            <NavLink to="/signup">Sign up</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     );
 };
 
