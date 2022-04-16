20f6ce32140378533a293523f606a040893a215e
sajibur-rahman <sajiburr21@gmail.com>
2022-04-16 11:13:21 +0600

    signup page is done

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
+import Button from "../Shared/BigButton/Button";
+import TextField from "../Shared/TextField/TextField";
 
 const Signup = () => {
-    return (
-        <div>
-            <h1>this is signup page</h1>
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center">
+      <div className="space-y-8">
+        <img className="w-52" src={logo} alt="" />
+        <form className="space-y-8">
+          <TextField placeholder="name" type="text" />
+          <TextField placeholder="email" type="email" />
+          <TextField placeholder="password" type="password" />
+          <Button>sign up</Button>
+          <p>
+            already regesterd ?{" "}
+            <Link className="text-red-500" to="/login">
+              login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
